37978e9b853f085aebd110323db155f940bc5888
Breno Guilherme de Souza Lopes <brenogdev@gmail.com>
2022-07-21 16:45:47 -0300

    (feat): criado aplicação

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { CssBaseline } from "@mui/material";
+import React from "react";
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
+};
